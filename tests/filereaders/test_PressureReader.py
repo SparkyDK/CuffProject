867bbccee0d1e20357755897741424f94fb856a1
SparkyDK <42686689+SparkyDK@users.noreply.github.com>
2018-08-26 17:34:16 -0400

    Got rid of a assertequals depracation error message

--- tests/filereaders/test_PressureReader.py
+++ tests/filereaders/test_PressureReader.py
@@ -5,6 +5,6 @@ import os
 class PressureTestReaderCase(unittest.TestCase):
     def test_canReadPressureFile(self):
         actions = PressureReader().read("./tests/test_files/TEST_Pressure_Values.txt")
-        self.assertEquals([['PMAX', '970'], ['PAINH', '920'], ['PAINL', '900'], ['PATM', '750']], actions)
+        self.assertEqual([['PMAX', '970'], ['PAINH', '920'], ['PAINL', '900'], ['PATM', '750']], actions)
 if __name__ == '__main__':
     unittest.main()
