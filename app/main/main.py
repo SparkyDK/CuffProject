7d0c6f4984d1d6b6adc8b78e15bd34fc52a4e088
SparkyDK <42686689+SparkyDK@users.noreply.github.com>
2018-08-27 01:10:50 -0400

    Updated Main with all function calls.  Still need to validate amount of time executing each function

--- app/main/main.py
+++ app/main/main.py
@@ -2,20 +2,21 @@ from app.constants.CONSTANTS import MAX_NUM_PARAMETERS
 from app.filereaders.ScheduleReader import ScheduleReader
 from app.filereaders.PressureReader import PressureReader
 
-DEBUG = True
-Global_cnt = 0
-past_states = [] * 5
-pain_required = False
-control_args = {'GO' : 0, 'STOP': 0,  'PAIN': 0,  'P': Patm,  'RUNNING': 0,  'PAUSE':0}
-current_counter = [] * max_num_schedules
-schedule_index = 0
-
 # Returns the user-provided pressure parameter values as a dictionary with keys of PMAX, PAINL, PAINH, PATM
 pressure_parameters = PressureReader().read(filename="./tests/input_files/Pressure_Values.txt")
 
 # Returns an array of tuples, with the desired action of Pain/Nil and the duration of each of those actions
 schedule = ScheduleReader().read(time_schedule, filename="./tests/input_files/Schedule.txt")
 max_num_schedules = len(schedule)
+
+DEBUG = True
+Global_cnt = 0
+past_states = [] * 5
+pain_required = False
+current_counter = [] * max_num_schedules
+control_args = {'SCHEDULE_INDEX': 0, 'PAIN': 0, 'STARTED':0, 'PAUSE':0, 'FORCE':0}
+user_args = {'GO' : 0, 'STOP': 0, 'ABORT': 0, 'override_pressure': pressure_parameters['PATM'], 'OVERRIDE':0}
+current_pressure = None
 
 try:
     # Create the system state machine
@@ -33,7 +34,7 @@ elapsed_time = 0
 while (True == True):
 
     # Keep a state history
-    returned_state =   sm.FSM.GetCurState()
+    returned_state = sm.FSM.GetCurState()
     # pop out the highest-index entry from the state history
     past_states.popleft()
     # Add the newest state value to the lowest-index entry of the state history
@@ -43,18 +44,27 @@ while (True == True):
     old_elapsed_time = elapsed_time
     elapsed_time = time.time() - start_time
     if ( math.floor(elapsed_time) != math.floor(old_elapsed_time) ):
-        # Only process the schedule every new second
-        second_tickover = True
-        pain_required = schedule.Execute(control_args)
-    else:
-        second_tickover = False
+        # Only process the schedule every new second tick
+        # Update the PAIN, STARTED, SCHEDULE_INDEX, PAUSE fields of the control arguments and current_counter values
+        (control_args) = schedule.Execute(current_counter, control_args, user_args)
 
-    # Poll for user input and update the GUI
-    (control_args) = GUI.Execute
+    # Read the current air pressure in the patient's cuff
+    current_pressure = Read_Cuff_Pressure()
 
-    # Execute the state machine
+    # Poll for user input and update the GUI based on the control arguments
+    # Then update the user signals: {'GO','STOP','ABORT','override_pressure','OVERRIDE'} appropriately
+    (user_args) = GUI.Execute(current_counter, control_args, user_args)
+
+    # Update or override the control signals: {'PAIN','STARTED','SCHEDULE_INDEX','PAUSE','FORCE'}
+    # NOTE: A FORCE is equivalent to an untimed PAIN cycle in PAUSE mode (PAUSE alone makes pressure NIL, otherwise)
+    #       OVERRIDE_PRESSURE (sampled from user_args) is used to create new values of PAINH and PAINL
+    #       STOP and ABORT both do an ABORT in the FORCE mode of operation, venting pressure, resetting index, etc.
+    old_control_args = control_args
+    (control_args) = state_machine_overrides(current_counter, control_args, user_args)
+
+    # Execute the state machine with the new control signals and pressure value
     try:
-        sm.Execute( control_args )
+        sm.Execute(current_pressure, control_args )
     except KeyboardInterrupt:
         print ("\nDone")
 
