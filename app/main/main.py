de4194c72800812a9fc9f57ae841a7b76c953a4d
SparkyDK <42686689+SparkyDK@users.noreply.github.com>
2018-08-26 21:12:54 -0400

    Added main program

--- app/main/main.py
+++ app/main/main.py
@@ -1,4 +1,6 @@
-from app.constants.CONSTANTS import MAX_NUM_SCHEDULES
+from app.constants.CONSTANTS import MAX_NUM_SCHEDULES, MAX_NUM_PARAMETERS
+from app.filereaders.ScheduleReader import ScheduleReader
+from app.filereaders.PressureReader import PressureReader
 
 DEBUG = True
 Global_cnt = 0
@@ -19,6 +21,15 @@ Patm = 0 #atmospheric pressure
 P = 0 #current pressure
 Pnew = 0 #new pressure value)
 
+schedule = ScheduleReader().read(time_schedule, filename="./tests/input_files/Schedule.txt")
+pressure_parameters = PressureReader().read(filename="./tests/input_files/Pressure_Values.txt")
+
 # Only the following parameter labels are supported and these are defined in this dictionary
-d = dict(PATM=0, PAINH=0, PAINL=0, PMAX=0)
-Max_num_Parameters = len(d)
+pressure_parameters = dict(PATM=0, PAINH=0, PAINL=0, PMAX=0)
+Max_num_Parameters = len(pressure_parameters)
+# Could use the constant instead!  Safer to keep the dictionary as the master for the parameters
+
+User_input = dict(STOP=1, ABORT=1, ENTER=0, GO=0, New_Pressure=pressure_parameters(Patm))
+
+
+
