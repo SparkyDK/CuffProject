c287fb51f1603ad9735bf965619c3a5e8a5da2fc
NS-10 <42972123+NS-10@users.noreply.github.com>
2018-09-13 16:10:58 -0400

    Absolute file path used

--- app/main/main.py
+++ app/main/main.py
@@ -186,14 +186,14 @@ for i in range(0, MAX_NUM_SCHEDULES):
     imported_schedule.append([])
 
 # Returns the user-provided pressure parameter values as a dictionary with keys of PMAX, PAINL, PAINH, PATM
-pressure_parameters = PressureReader().read(filename="./app/input_files/Pressure_Values.txt")
+pressure_parameters = PressureReader().read(filename="C:/Users/natha/Documents/GitHub/CuffProject/app/input_files/Pressure_Values.txt")
 painl = int(pressure_parameters['PAINVALUE'] - pressure_parameters['PAINTOLERANCE'])
 painh = int(pressure_parameters['PAINVALUE'] + pressure_parameters['PAINTOLERANCE'])
 
 print ("pressure_parameters", pressure_parameters, "painh=", painh, "and painl=", painl)
 
 # Returns an array of tuples, with the desired action of Pain/Nil and the duration of each of those actions
-imported_schedule = ScheduleReader().read( filename="./app/input_files/Schedule.txt", file_schedule=imported_schedule )
+imported_schedule = ScheduleReader().read( filename="C:/Users/natha/Documents/GitHub/CuffProject/app/input_files/Schedule.txt", file_schedule=imported_schedule )
 max_num_schedules = len(imported_schedule)
 print ("main read imported_schedule:", imported_schedule)
 
