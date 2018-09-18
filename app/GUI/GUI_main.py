8bf973fe6f5493b1110fa3150d85b23bacfb1090
SparkyDK <42686689+SparkyDK@users.noreply.github.com>
2018-09-18 00:49:33 -0400

    Seems to be executing both GUI and control code... not sure if variables are being updated properly yet....

--- app/GUI/GUI_main.py
+++ app/GUI/GUI_main.py
@@ -2,8 +2,11 @@ from kivy.config import Config
 Config.set('kivy', 'keyboard_mode', 'systemandmulti')
 #kivy.require("1.10.1")
 
+from functools import partial
+
 from kivy.uix.floatlayout import FloatLayout
 from kivy.app import App
+from kivy.clock import Clock
 
 from datetime import datetime
 from app.System.pressure_measurement.pressure_sampling import Read_Cuff_Pressure
@@ -19,47 +22,72 @@ from app.constants.CONSTANTS import HISTORY_LENGTH
 
 import math
 import time
+import threading
 
 class Display(FloatLayout):  # intro <display> and tells actions/functions
     def __init__(self, **kwargs):
         super(Display, self).__init__(**kwargs)
         #print(kwargs)
         self.txt = 170
-        print ("Started up the Display and ran __init__")
-        time.sleep(2)
+        print ("Started up the Display")
 
-        # control_args = {}           # SCHEDULE_INDEX','PAIN','STARTED','PAUSE','PAINH','PAINL','PRESSURE','PATM','PMAX'
-        # user_args = {}              # 'GO','STOP','ABORT','UP','DOWN','override_pressure','OVERRIDE'
-        # pressure_parameters = {}    # 'PAINTOLERANCE', 'PAINVALUE', 'PATM', 'PMAX'
-        # imported_schedule = []      # MAX_NUM_SCHEDULES tuples of: {NIL|PAIN, seconds_value}
-        # schedule_finished = 0       # boolean to indicate pain schedule is complete
-        #
-        # past_states = collections.deque([None]*HISTORY_LENGTH)            # queue history of past few states
-        # start_time = None           # initial system starting time for program
-        # elapsed_time = 0            # program execution time
-        #
-        # Global_cnt=0                # Counter to keep track of number of loops in the while(true): construct
-        # toggle = 0                  # used for keyboard-based debugging
-        # current_counter=0           # keeps track of seconds count for current phase of pain schedule
-        #
-        # decision = ControlDecisions()
-        # airctrl = Setup_FSM_States()    # state machine to control relays
-        # schedule = pain_schedule()      # manages the NIL/PAIN schedule
-        #
-        # # Initialize the system
-        # control_args, user_args, pressure_parameters, schedule_finished, start_time, elapsed_time, \
-        # current_counter, imported_schedule, Global_cnt, past_states, decision, airctrl, schedule, toggle = \
-        #     self.setup_system(control_args, user_args, pressure_parameters, schedule_finished, start_time,
-        #                       elapsed_time, current_counter, imported_schedule, Global_cnt, past_states,
-        #                       decision, airctrl, schedule, toggle)
-        # #while (True):
-        # if (True==True):
-        #     # Run the system forever
-        #     control_args, user_args, pressure_parameters, schedule_finished, start_time, elapsed_time, \
-        #     current_counter, imported_schedule, Global_cnt, past_states, decision, airctrl, schedule, toggle = \
-        #         self.run_system(control_args, user_args, pressure_parameters, schedule_finished, start_time,
-        #                         elapsed_time, current_counter, imported_schedule, Global_cnt, past_states,
-        #                         decision, airctrl, schedule, toggle)
+        localtime = time.asctime(time.localtime(time.time()))
+        print ("Display:",localtime)
+
+    def count(self, *varargs):
+        pass
+        #self.start = datetime.now()
+        #Clock.schedule_interval(self.on_timeout, 1)
+
+    def on_timeout(self, *args):
+        d = datetime.now() - self.start
+        self.ids.schedule1.text = datetime.utcfromtimestamp(d.total_seconds()).strftime("%S")
+
+    def abort_function(self):
+        print ("Aborting...")
+        self.ids.abort.text = "Clicked"
+        #user_args{'ABORT'} = 1
+
+
+    def enter_function(self):
+        self.ids.enter.text = "Clicked"
+
+    def go_function(self):
+        print ("Starting a schedule")
+        self.ids.go.text = "Clicked"  # changes go to clicked when clicked
+
+    def stop_function(self):
+        print ("Stopping/pausing a schedule")
+        self.ids.stop.text = "Clicked"
+
+    def newpressureup(self):
+        pass
+
+    def newpressure_function(self, direction, desired_pressure):
+        #app = App.get_running_app()
+        # self.direction = direction
+        # self.desired_pressure = desired_pressure
+
+        if (direction == "up"):
+            pass
+            #app.desired_pressure += 1
+        elif (direction == "down"):
+            pass
+            #app.desired_pressure -= 1
+        else:
+            print("Error! newpressure_function: We should not be here (expecting 'up' or 'down' only)")
+        pass
+
+    # def do_something(self, args, dt, *largs):
+    #     self.args = args
+    #     self.args['result'] += 1
+    #     print ("TEST: ",self.args['result'])
+    #     #print ("arguments: args=", args, "and dt=",dt)
+    #     print ("do_something args:", args)
+    #     localtime = time.asctime(time.localtime(time.time()))
+    #     print("\ndo_something:", localtime)
+    #     #time.sleep(1)
+    #     return (localtime, 4)
 
     def setup_system(self, control_args, user_args, pressure_parameters, schedule_finished, start_time, elapsed_time,
                      current_counter, imported_schedule, Global_cnt, past_states, decision, airctrl, schedule, toggle):
@@ -108,29 +136,19 @@ class Display(FloatLayout):  # intro <display> and tells actions/functions
                self.current_counter, self.imported_schedule, self.Global_cnt, self.past_states,
                self.decision, self.airctrl, self.schedule, self.toggle)
 
-    def run_system(self, control_args, user_args, pressure_parameters, schedule_finished, start_time, elapsed_time,
-                     current_counter, imported_schedule, Global_cnt, past_states, decision, airctrl, schedule, toggle):
-        self.control_args = control_args
-        self.user_args = user_args
-        self.pressure_parameters = pressure_parameters
-        self.schedule_finished = schedule_finished
-        self.start_time = start_time
-        self.elapsed_time = elapsed_time
-        self.current_counter = current_counter
-        self.imported_schedule = imported_schedule
-        self.Global_cnt = Global_cnt
-        self.past_states = past_states
-        self.decision = decision
-        self.airctrl = airctrl
-        self.schedule = schedule
-        self.toggle = toggle
+    def run_system(self, args, dt, *largs):
 
+        print ("run_system args:", args)
+
+        self.control_args, self.user_args, self.pressure_parameters, self.schedule_finished, self.start_time,\
+        self.elapsed_time, self.current_counter, self.imported_schedule, self.Global_cnt, self.past_states,\
+        self.decision, self.airctrl, self.schedule, self.toggle = args
 
         # Not particularly necessary; mostly for debugging purposes
         self.Global_cnt += 1
 
         # Keep a state history
-        returned_state = airctrl.FSM.GetCurState()
+        returned_state = self.airctrl.FSM.GetCurState()
         # pop out the highest-index entry from the state history
         self.past_states.popleft()
         # Add the newest state value to the lowest-index entry of the state history
@@ -138,7 +156,7 @@ class Display(FloatLayout):  # intro <display> and tells actions/functions
 
         #localtime = time.asctime(time.localtime(time.time()))
         old_elapsed_time = self.elapsed_time
-        self.elapsed_time = time.time() - start_time
+        self.elapsed_time = time.time() - self.start_time
 
         # Pain schedule requires a one-second tick, created at the point where the integer truncation
         # of system time seconds ticks over to the next integer value (e.g. 3.99999992 becomes 4.0000245)
@@ -154,7 +172,7 @@ class Display(FloatLayout):  # intro <display> and tells actions/functions
         # Then update the user signals: {'GO','STOP','ABORT','override_pressure','OVERRIDE'} appropriately
 
         try:
-            old_control_args = control_args.copy()
+            #old_control_args = control_args.copy()
             # Update or override the control signals: {'PAIN','STARTED','SCHEDULE_INDEX','PAUSE'}
             # Execute the asynchronous part of the state machine that implements the control decisions
             # with the newly-updated control signals and newly-sampled pressure value
@@ -165,73 +183,65 @@ class Display(FloatLayout):  # intro <display> and tells actions/functions
                 self.decision.respond_to_user_inputs(self.current_counter, self.imported_schedule, self.control_args,
                                                      self.user_args, self.pressure_parameters, second_tickover,
                                                      self.schedule_finished, self.airctrl,
-                                                     self.schedule, toggle)
+                                                     self.schedule, self.toggle)
             if (self.schedule_finished == True):
                 # At the end of the pain schedule, turn off pain and keep venting the cuff in IDLE state
-                control_args['PAIN'] = 0
+                self.control_args['PAIN'] = 0
 
             # Execute the state machine
-            airctrl.FSM.Execute(control_args)
+            self.airctrl.FSM.Execute(self.control_args)
 
         except KeyboardInterrupt:
             print("\nDone")
 
-        return(self.control_args, self.user_args, self.pressure_parameters, self.elapsed_time, self.start_time,
-               self.schedule_finished, self.current_counter, self.imported_schedule, self.Global_cnt, self.past_states,
-               self.decision, self.airctrl, self.schedule, self.toggle)
-
-    def count(self, *varargs):
-        pass
-        #self.start = datetime.now()
-        #Clock.schedule_interval(self.on_timeout, 1)
-
-    def on_timeout(self, *args):
-        d = datetime.now() - self.start
-        self.ids.schedule1.text = datetime.utcfromtimestamp(d.total_seconds()).strftime("%S")
-
-    def abort_function(self):
-        print ("Aborting...")
-        self.ids.abort.text = "Clicked"
-        #user_args{'ABORT'} = 1
-
-
-    def enter_function(self):
-        self.ids.enter.text = "Clicked"
-
-    def go_function(self):
-        print ("Starting a schedule")
-        self.ids.go.text = "Clicked"  # changes go to clicked when clicked
-
-    def stop_function(self):
-        print ("Stopping/pausing a schedule")
-        self.ids.stop.text = "Clicked"
-
-    def newpressureup(self):
-        pass
-
-    def newpressure_function(self, direction, desired_pressure):
-        #app = App.get_running_app()
-        # self.direction = direction
-        # self.desired_pressure = desired_pressure
-
-        if (direction == "up"):
-            pass
-            #app.desired_pressure += 1
-        elif (direction == "down"):
-            pass
-            #app.desired_pressure -= 1
-        else:
-            print("Error! newpressure_function: We should not be here (expecting 'up' or 'down' only)")
-        pass
+        # return(self.control_args, self.user_args, self.pressure_parameters, self.elapsed_time, self.start_time,
+        #        self.schedule_finished, self.current_counter, self.imported_schedule, self.Global_cnt, self.past_states,
+        #        self.decision, self.airctrl, self.schedule, self.toggle)
 
 class DisplayApp(App):  # defines app and returns display
-    disp = Display()
-    print ("disp:", disp)
+    #print ("disp:", disp)
+    testvar = 0
 
     def build(self):
-        print ("built Display")
-        return Display()
-        #return disp
+
+        control_args = {}  # SCHEDULE_INDEX','PAIN','STARTED','PAUSE','PAINH','PAINL','PRESSURE','PATM','PMAX'
+        user_args = {}  # 'GO','STOP','ABORT','UP','DOWN','override_pressure','OVERRIDE'
+        pressure_parameters = {}  # 'PAINTOLERANCE', 'PAINVALUE', 'PATM', 'PMAX'
+        imported_schedule = []  # MAX_NUM_SCHEDULES tuples of: {NIL|PAIN, seconds_value}
+        schedule_finished = 0  # boolean to indicate pain schedule is complete
+
+        past_states = collections.deque([None] * HISTORY_LENGTH)  # queue history of past few states
+        start_time = None  # initial system starting time for program
+        elapsed_time = 0  # program execution time
+
+        Global_cnt = 0  # Counter to keep track of number of loops in the while(true): construct
+        toggle = 0  # used for keyboard-based debugging
+        current_counter = 0  # keeps track of seconds count for current phase of pain schedule
+
+        decision = ControlDecisions()
+        airctrl = Setup_FSM_States()  # state machine to control relays
+        schedule = pain_schedule()  # manages the NIL/PAIN schedule
+
+        disp = Display()
+
+        # Initialize the system
+        control_args, user_args, pressure_parameters, schedule_finished, start_time, elapsed_time, \
+        current_counter, imported_schedule, Global_cnt, past_states, decision, airctrl, schedule, toggle = \
+            disp.setup_system(control_args, user_args, pressure_parameters, schedule_finished, start_time,
+                             elapsed_time, current_counter, imported_schedule, Global_cnt, past_states,
+                             decision, airctrl, schedule, toggle)
+
+        Clock.schedule_interval(partial(disp.run_system, (control_args, user_args, pressure_parameters,
+                                                          schedule_finished, start_time, elapsed_time, current_counter,
+                                                          imported_schedule, Global_cnt, past_states, decision, airctrl,
+                                                          schedule, toggle) ), 1.0 )
+                                                          #  schedule, toggle) ), 1.0 / 60.0)
+
+        # result=0
+        # localtime = time.asctime(time.localtime(time.time()))
+        # Clock.schedule_interval(partial(disp.do_something, {'time':localtime, 'result':result} ), 1.0/60.0)
+
+        return disp
 
 if __name__ == '__main__':
     gui = DisplayApp()
