4a14539e10aeff0b1907bdf763923476aec84c51
SparkyDK <42686689+SparkyDK@users.noreply.github.com>
2018-10-18 20:21:06 -0400

    One shot read to try and sync ADC

--- app/System/A_to_D/ADC_sampling.py
+++ app/System/A_to_D/ADC_sampling.py
@@ -122,7 +122,10 @@ class ADC_sampling:
         # Using code taken from: https://github.com/ul-gh/PiPyADC/blob/master/pipyadc.py
         # Fill the buffer first once before displaying continuously updated results
 
-        one_pressure_sample = ads2.read_oneshot(PRESSURE)
+        while (True == True):
+            one_pressure_sample = ads2.read_oneshot(PRESSURE)
+            print ("Read a single pressure value =", one_pressure_sample)
+
         if (one_pressure_sample != 0):
             print ("Read a single pressure value =", one_pressure_sample)
 
