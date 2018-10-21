1064ede2041e97319f81d8a03cab75b0d8625abb
SparkyDK <42686689+SparkyDK@users.noreply.github.com>
2018-10-20 23:37:51 -0400

    test

--- app/GUI/GUI_main.py
+++ app/GUI/GUI_main.py
@@ -316,7 +316,7 @@ class Display(Screen):  # intro <display> and tells actions/functions
     def new_pressure_down(self):
         g.user_args['override_pressure'] -= 1
         print ("Decreasing pressure to", g.user_args['override_pressure'])
-        set_relay(s1="open", s2="closed", s3="open")
+        set_relay(s1="closed", s2="open", s3="closed)
         time.sleep (2)
 
     def new_pressure_up(self):
