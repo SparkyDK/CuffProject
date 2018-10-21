d5973e31ff8fb028eeca97bab0253a1e39465af2
SparkyDK <42686689+SparkyDK@users.noreply.github.com>
2018-10-21 01:04:06 -0400

    test

--- app/GUI/GUI_main.py
+++ app/GUI/GUI_main.py
@@ -285,8 +285,8 @@ class Display(Screen):  # intro <display> and tells actions/functions
 
     def abort_ack_function(self):
         self.ids.abort.text = ""
-        set_relay(s1="open", s2="open", s3="closed")
-        time.sleep (2)
+        #set_relay(s1="open", s2="open", s3="closed")
+        #time.sleep (2)
 
     def abort_function(self):
         print ("Requesting an Abort ...")
@@ -299,8 +299,8 @@ class Display(Screen):  # intro <display> and tells actions/functions
     def enter_function(self):
         self.ids.enter.text = "ENTER"
         g.user_args['OVERRIDE'] = 1
-        set_relay(s1="open", s2="closed", s3="closed")
-        time.sleep (2)
+        #set_relay(s1="open", s2="closed", s3="closed")
+        #time.sleep (2)
 
     def go_ack_function(self):
         self.ids.go.text = ""
@@ -317,20 +317,20 @@ class Display(Screen):  # intro <display> and tells actions/functions
         g.user_args['STOP'] = 1
         print ("Requesting the Stopping/pausing of a schedule")
         self.ids.stop.text = "STOP"
-        set_relay(s1="closed", s2="closed", s3="closed")
-        time.sleep (2)
+        #set_relay(s1="closed", s2="closed", s3="closed")
+        #time.sleep (2)
 
     def new_pressure_down(self):
         g.user_args['override_pressure'] -= 1
         print ("Decreasing pressure to", g.user_args['override_pressure'])
-        set_relay(s1="closed", s2="open", s3="closed")
-        time.sleep (2)
+        #set_relay(s1="closed", s2="open", s3="closed")
+        #time.sleep (2)
 
     def new_pressure_up(self):
         g.user_args['override_pressure'] += 1
         print ("Increasing pressure to", g.user_args['override_pressure'])
-        set_relay(s1="closed", s2="open", s3="open")
-        time.sleep (2)
+        #set_relay(s1="closed", s2="open", s3="open")
+        #time.sleep (2)
 
     def switch_function(self):
         self.ids.select.text = "ENTER"
