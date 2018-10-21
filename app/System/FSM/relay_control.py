a20f067d315d79ddb4a1800ff1d4e96afca7af19
SparkyDK <42686689+SparkyDK@users.noreply.github.com>
2018-10-20 22:34:01 -0400

    test

--- app/System/FSM/relay_control.py
+++ app/System/FSM/relay_control.py
@@ -20,10 +20,10 @@ def set_relay(s1, s2, s3):
 
      # Solenoid that vents the "reservoir" section of tubing in the box
      if (s2 == "open" or s2 == "OPEN"):
-         # Assume normally closed relay
-         wiringpi.digitalWrite(3, 1) # sets port 2 to 1 (3V3, on)
-     elif(s2 == "closed" or s2 == "CLOSED"):
+         # Assume normally open relay
          wiringpi.digitalWrite(3, 0) # sets port 2 to 0 (0V, off)
+     elif(s2 == "closed" or s2 == "CLOSED"):
+         wiringpi.digitalWrite(3, 1) # sets port 2 to 1 (3V3, on)
      else:
          print("s2 can be either 'open' or 'closed', but it has been set to:", s2)
          exit(0)
