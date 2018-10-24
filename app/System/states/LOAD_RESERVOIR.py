d98f56c16ced04142b2cd02eb0442f307782313e
SparkyDK <42686689+SparkyDK@users.noreply.github.com>
2018-10-23 23:07:12 -0400

    test

--- app/System/states/LOAD_RESERVOIR.py
+++ app/System/states/LOAD_RESERVOIR.py
@@ -56,7 +56,7 @@ class LOAD_RESERVOIR(State):
         # close all of the relays
         # S1 Closed, S2 Closed, S3 Closed
         set_relay(s1="closed", s2="closed", s3="closed")
-        time.sleep(relay_settling_time)  # Give the relays and solenoids time to actually close
+        time.sleep(relay_settling_time+0.5)  # Give the relays and solenoids time to actually close
         # need to determine this value, by experiment, but they are specified as having a response time less than 20ms
         #print("Exiting Load Reservoir")
         #time.sleep(cuff_charging_time)
