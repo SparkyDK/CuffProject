36e407d002dd654c4a03934e9620882efea7397e
SparkyDK <42686689+SparkyDK@users.noreply.github.com>
2018-10-25 03:03:40 -0400

    test

--- app/System/FSM/control_decisions.py
+++ app/System/FSM/control_decisions.py
@@ -121,9 +121,9 @@ class ControlDecisions:
             # All other cases, do nothing.
             # This includes initial power-up and the final case when the schedule is finished
             # Also includes cases where GO and STOP buttons are ignored appropriately
-            print ("Not doing anything with STARTED=",self.control_args['STARTED'], "PAUSE=",\
-                   self.control_args['PAUSE'], "STOP=", self.user_args['STOP'], "GO=", self.user_args['GO'],\
-                   "ABORT=", self.user_args['ABORT'])
+            #print ("Not doing anything with STARTED=",self.control_args['STARTED'], "PAUSE=",\
+            #       self.control_args['PAUSE'], "STOP=", self.user_args['STOP'], "GO=", self.user_args['GO'],\
+            #       "ABORT=", self.user_args['ABORT'])
             if (self.second_tickover == True):
                 self.user_args['GO'] = 0
                 self.user_args['STOP'] = 0
