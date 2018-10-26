a700f661caae53a9470d2074a9450b653a370981
SparkyDK <42686689+SparkyDK@users.noreply.github.com>
2018-10-25 22:13:58 -0400

    test

--- app/GUI/GUI_main.py
+++ app/GUI/GUI_main.py
@@ -158,8 +158,7 @@ class Display(Screen):  # intro <display> and tells actions/functions
             print("\n***** Reading pressure in cuff to have a digital value of ", self.raw_average, " *****")
             self.atmospheric_pressure = str(g.pressure_parameters['PATM'])
             debug_msg = str( localtime + ": Measured atmospheric digital value=" + self.raw_average +\
-                             ", displayed as: " + self.displayed_pressure + " mm Hg (atm=" +\
-                             self.atmospheric_pressure + ")" )
+                             " is " + self.displayed_pressure + " mm Hg (with atm=" + self.atmospheric_pressure + ")" )
             g.my_logger.debug(debug_msg)
 
             #event = Clock.schedule_interval(partial(self.run_system, (g.control_args, g.user_args,\
