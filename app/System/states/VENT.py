581115d52e9d6dd9b2707a06f4e1359dc651d7fb
SparkyDK <42686689+SparkyDK@users.noreply.github.com>
2018-10-26 00:28:17 -0400

    test

--- app/System/states/VENT.py
+++ app/System/states/VENT.py
@@ -21,7 +21,7 @@ class VENT(State):
             #set_relay(s1="closed", s2="open", s3="open")
             # Stay in the VENT state
             print("Stay in vent because pressure=", self.args['PRESSURE'],\
-            #      " is greater than atmospheric pressure", self.args['PATM'])
+                  " is greater than atmospheric pressure", self.args['PATM'], "+ tolerance=", ATM_TOLERANCE)
         else:
             self.FSM.set_SYNC()
             self.FSM.ToTransition("toIDLE")
