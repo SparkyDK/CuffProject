1091a681ec871d9991ea897e7b32adc115776fea
SparkyDK <42686689+SparkyDK@users.noreply.github.com>
2018-10-27 21:21:04 -0400

    Increased pressure settling time parameter

--- app/constants/CONSTANTS.py
+++ app/constants/CONSTANTS.py
@@ -19,7 +19,7 @@ MAX_NUM_PARAMETERS = 4
 
 refresh_period = 0.1
 relay_settling_time = 0.04
-pressure_settling_time = 0.3
+pressure_settling_time = 0.75
 venting_timeout = 15
 cuff_charging_time = 0.005
 
